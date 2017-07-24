62eb4a074121c91f91a970daf123f33ff5091b32
Claire Miller <millerc2@student.unimelb.edu.au>
2017-07-24 16:30:42 +1000

    Removed mutation overwrite for the proliferative types writer

--- cell_based/src/writers/cell_writers/CellProliferativeTypesWriter.cpp
+++ cell_based/src/writers/cell_writers/CellProliferativeTypesWriter.cpp
@@ -51,23 +51,22 @@ template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 double CellProliferativeTypesWriter<ELEMENT_DIM, SPACE_DIM>::GetCellDataForVtkOutput(CellPtr pCell, AbstractCellPopulation<ELEMENT_DIM, SPACE_DIM>* pCellPopulation)
 {
     double colour = pCell->GetCellProliferativeType()->GetColour();
-
     // Set colour dependent on cell mutation state
-    if (!pCell->GetMutationState()->IsType<WildTypeCellMutationState>())
-    {
-        colour = pCell->GetMutationState()->GetColour();
-    }
-    if (pCell->HasCellProperty<CellLabel>())
-    {
-        CellPropertyCollection collection = pCell->rGetCellPropertyCollection().GetProperties<CellLabel>();
-        boost::shared_ptr<CellLabel> p_label = boost::static_pointer_cast<CellLabel>(collection.GetProperty());
-        colour = p_label->GetColour();
-    }
-    if (pCell->HasCellProperty<ApoptoticCellProperty>() || pCell->HasApoptosisBegun())
-    {
-        ///\todo: replace this hard-coded 6 with the ApoptoticCellProperty member mColour?
-        colour = 6.0;
-    }
+    // if (!pCell->GetMutationState()->IsType<WildTypeCellMutationState>())
+    // {
+    //     colour = pCell->GetMutationState()->GetColour();
+    // }
+    // if (pCell->HasCellProperty<CellLabel>())
+    // {
+    //     CellPropertyCollection collection = pCell->rGetCellPropertyCollection().GetProperties<CellLabel>();
+    //     boost::shared_ptr<CellLabel> p_label = boost::static_pointer_cast<CellLabel>(collection.GetProperty());
+    //     colour = p_label->GetColour();
+    // }
+    // if (pCell->HasCellProperty<ApoptoticCellProperty>() || pCell->HasApoptosisBegun())
+    // {
+    //     ///\todo: replace this hard-coded 6 with the ApoptoticCellProperty member mColour?
+    //     colour = 6.0;
+    // }
 
     return colour;
 }
@@ -77,22 +76,22 @@ void CellProliferativeTypesWriter<ELEMENT_DIM, SPACE_DIM>::VisitCell(CellPtr pCe
 {
     unsigned colour = pCell->GetCellProliferativeType()->GetColour();
 
-    // Set colour dependent on cell mutation state
-    if (!pCell->GetMutationState()->IsType<WildTypeCellMutationState>())
-    {
-        colour = pCell->GetMutationState()->GetColour();
-    }
-    if (pCell->HasCellProperty<CellLabel>())
-    {
-        CellPropertyCollection collection = pCell->rGetCellPropertyCollection().GetProperties<CellLabel>();
-        boost::shared_ptr<CellLabel> p_label = boost::static_pointer_cast<CellLabel>(collection.GetProperty());
-        colour = p_label->GetColour();
-    }
-    if (pCell->HasCellProperty<ApoptoticCellProperty>() || pCell->HasApoptosisBegun())
-    {
-        ///\todo: replace this hard-coded 6 with the ApoptoticCellProperty member mColour? (#2512)
-        colour = 6;
-    }
+    // // Set colour dependent on cell mutation state
+    // if (!pCell->GetMutationState()->IsType<WildTypeCellMutationState>())
+    // {
+    //     colour = pCell->GetMutationState()->GetColour();
+    // }
+    // if (pCell->HasCellProperty<CellLabel>())
+    // {
+    //     CellPropertyCollection collection = pCell->rGetCellPropertyCollection().GetProperties<CellLabel>();
+    //     boost::shared_ptr<CellLabel> p_label = boost::static_pointer_cast<CellLabel>(collection.GetProperty());
+    //     colour = p_label->GetColour();
+    // }
+    // if (pCell->HasCellProperty<ApoptoticCellProperty>() || pCell->HasApoptosisBegun())
+    // {
+    //     ///\todo: replace this hard-coded 6 with the ApoptoticCellProperty member mColour? (#2512)
+    //     colour = 6;
+    // }
 
     *this->mpOutStream << colour << " ";
 }
